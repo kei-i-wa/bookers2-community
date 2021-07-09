9c7bfff70bcfbaaba92558f715a3b3a6a80fa0b5
kei-i-wa <kei.kumarg@gmail.com>
2021-07-09 13:19:51 +0000

    メール送信機能

--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,7 @@ Rails.application.routes.draw do
   # get'users' =>'users#index'
   resources :groups do
     get "join" =>"groups#join"
+    get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
   end
   end
